6e29f929739d31157c84d56ae0ee77ec7113e0d5
Vinicius Augutis <viniciusaugutis@gmail.com>
2021-02-06 17:33:03 -0300

    Incluindo botão de calcular

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,23 @@ class HomePage extends StatelessWidget {
         body: ListView(children: <Widget>[
           Logo(),
           Input(ctr: _gasCtrl, label: "Gasolina"),
-          Input(ctr: _alcCtrl, label: "Álcool")
+          Input(ctr: _alcCtrl, label: "Álcool"),
+          Container(
+              margin: EdgeInsets.all(30),
+              height: 80,
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(60)),
+              child: FlatButton(
+                child: Text(
+                  "CALCULAR",
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 25,
+                      fontFamily: 'Big Shoulders Display'),
+                ),
+                onPressed: () {},
+              ))
         ]));
   }
 }
