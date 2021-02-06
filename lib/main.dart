3e83ed5cd646f571b4d7c5eea77729b57df26f0e
Vinicius Augutis <viniciusaugutis@gmail.com>
2021-02-06 17:50:54 -0300

    Layout da mensagem de sucesso

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,45 @@ class HomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).primaryColor,
         body: ListView(children: <Widget>[
           Logo(),
+          Container(
+              margin: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(25)),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 30),
+                  Text(
+                    "Compensa utilizar álcool",
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 30,
+                        fontFamily: 'Big Shoulders Display'),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    margin: EdgeInsets.all(30),
+                    height: 60,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.8),
+                      borderRadius: BorderRadius.circular(60),
+                    ),
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        "CALCULAR NOVAMENTE",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 15,
+                          fontFamily: "Big Shoulders Display",
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )),
           Input(ctr: _gasCtrl, label: "Gasolina"),
           Input(ctr: _alcCtrl, label: "Álcool"),
           Container(
