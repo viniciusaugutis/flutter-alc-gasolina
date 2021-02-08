ce431b605a31c70717c518f326afe99d8602b154
Vinicius Augutis <viniciusaugutis@gmail.com>
2021-02-08 19:54:19 -0300

    Implementando cálculo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:alcoolgasolina/widgets/input.widget.dart';
-import 'package:alcoolgasolina/widgets/loading-button.widget.dart';
-import 'package:alcoolgasolina/widgets/logo.widget.dart';
-import 'package:alcoolgasolina/widgets/submit-form.widget.dart';
-import 'package:alcoolgasolina/widgets/sucess.widget.dart';
+import 'package:alcoolgasolina/pages/home.page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,33 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Álcool ou Gasolina',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  var _gasCtrl = new MoneyMaskedTextController();
-  var _alcCtrl = new MoneyMaskedTextController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        body: ListView(children: <Widget>[
-          Logo(),
-          SubmitForm(
-              gasCtrl: _gasCtrl,
-              alcCtrl: _alcCtrl,
-              busy: false,
-              submitFunc: () {}),
-          Success(result: "Compensa utilizar X", reset: () {})
-        ]));
+        title: 'Álcool ou Gasolina',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomePage());
   }
 }
